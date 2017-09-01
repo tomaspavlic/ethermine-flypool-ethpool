0f0e18aa7605f506e0734dedef4bca030bb22327
Tomáš Pavlič <tomas.pavlic@aps-holding.com>
2017-09-01 13:12:52 +0200

    add project with example console application.

---
+++ Topdev.Crypto.Pools.Generic.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Topdev.Crypto.Pools.Generic.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
